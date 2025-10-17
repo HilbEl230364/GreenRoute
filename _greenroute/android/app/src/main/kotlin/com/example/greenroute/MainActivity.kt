2f78b5d9862ec67337869207910f00c36689dcae
Elias Hilbel <40146720230364@litec.ac.at>
2025-10-17 09:51:00 +0200

    allesGelöscht

--- _greenroute/android/app/src/main/kotlin/com/example/greenroute/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.greenroute
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- _greenroute/lib/backend/route_service.dart
+++
@@ -1,6 +0,0 @@
-class RouteService {
-  // Dummy-Funktion: gibt eine Route zurück
-  static List<String> getRoute(String start, String ziel) {
-    return ['Start: $start', 'Zwischenstopp: Park', 'Ziel: $ziel'];
-  }
-}
